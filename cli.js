0582308ebf93a23791a5a722b143bd43f9453934
Alexander <alykoshin@gmail.com>
2019-02-20 18:26:05 +0300

    a lot of changes

--- cli.js
+++ cli.js
@@ -4,6 +4,8 @@
 var argv = require('minimist')(process.argv.slice(2));
 var pkg = require('./package.json');
 var shelling = require('./');
+var {lookup } = require('./lib/lookupError');
+var {gen_certs } = require('./lib/genSelfSignedCerts');
 
 
 function help() {
@@ -15,7 +17,14 @@ function help() {
     '',
     '  Example:',
     '    node node_modules/' + pkg.name + '/cli.js',
-    ''
+    '',
+    '  spawn:',
+    '    node node_modules/' + pkg.name + '/cli.js spawn {command}',
+    '  lookup:',
+    '    node node_modules/' + pkg.name + '/cli.js lookup NOENT',
+    '  gen_certs:',
+    '    node node_modules/' + pkg.name + '/cli.js gen_certs ./demo/config.json',
+    '',
   ].join('\n'));
 }
 
@@ -28,17 +37,52 @@ function version() {
   ].join('\n'));
 }
 
-if (argv.h || argv.help) {
-  help();
-  process.exit(0);
-} else if (argv.v || argv.version) {
-  version();
-  process.exit(0);
+async function handleArgv() {
+  if (argv.h || argv.help) {
+    help();
+    process.exit(0);
+  } else if (argv.v || argv.version) {
+    version();
+    process.exit(0);
+
+  } else if (process.argv[ 2 ] === 'gen_certs') {
+    const res = await gen_certs(process.argv[3]);
+    //console.log('*', res);
+    process.exit(0);
+
+  } else if (process.argv[ 2 ] === 'lookup') {
+    const code = process.argv[ 3 ];
+    console.log('*', lookup(code));
+    process.exit(0);
+
+  } else if (process.argv[ 2 ] === 'spawn') {
+    const cmd = process.argv.slice(3);
+    console.log('* COMMAND: "' + cmd + '"');
+    await shelling.spawnCapture(cmd)
+      .then(result => {
+        console.log('* RESULT: "' + result + '"');
+        process.exit(0);
+      })
+      .catch(e => {
+        console.log('* ERROR:');
+        if (e.code) {
+          const description = lookup(e.code);
+          if (!description.notFound) {
+            console.error('* ' + description.short);
+            console.error('* ' + description.long);
+          }
+        }
+        console.error('* ', e);
+        process.exit(-1);
+      })
+    ;
+  }
 }
 
+handleArgv();
 
-var main = require('./');
-
-main.exec(argv[0], function() {
-
-});
+//var main = require('./');
+//
+//main.exec(argv[0], function() {
+//
+//});
--- test/index.js
+++ test/index.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
-var chai = require('chai');
-var assert = chai.assert;
-var expect = chai.expect;
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
 chai.should();
-chai.use(require('chai-things')); //http://chaijs.com/plugins/chai-things
+//chai.use(require('chai-things')); // breaks .to.have.all.keys
+chai.use(require('chai-arrays'));
 
 
 describe('shelling', function () {
 
   before('before', function () {
-
   });
 
   beforeEach('beforeEach', function () {
@@ -27,9 +27,104 @@ describe('shelling', function () {
 
   });
 
-  it('should have unit test', function () {
+  describe('lookup', function () {
+    let lookup;
+    before('before', function () {
+      lookup = require('../lib/lookupError').lookup;
+    });
 
-    assert(false, 'Please add unit tests.');
+    it('is a function', function () {
+      assert(typeof lookup === 'function', 'Expect function');
+    });
+
+    it('handles common errors', function () {
+      const result = lookup('EACCES');
+      expect(result).to.be.an('object');
+      expect(result).to.include.all.keys('short', 'long');
+    });
+
+    it('handles linux errors', function () {
+      const result = lookup('EACCES');
+      expect(result).to.be.an('object');
+      expect(result).to.include.all.keys('short');
+    });
+
+    it('handles unknown errors', function () {
+      const result = lookup('__non__existent__');
+      expect(result).to.be.an('object');
+      expect(result).to.include.all.keys('short', 'long', 'notFound');
+      expect(result).to.include({ notFound: true });
+    });
+
+  });
+
+  describe('stringUtils', function () {
+    let stringUtils;
+
+    before('before', function () {
+      stringUtils = require('../lib/stringUtils');
+    });
+
+    describe('splitQuoted', function () {
+
+      it('is a function', function () {
+        assert(typeof stringUtils.splitQuoted === 'function', 'Expect function');
+      });
+
+      it('returns array with empty string for empty string', function () {
+        const data = '';
+        const expected = [''];
+        const result = stringUtils.splitQuoted(data);
+        expect( result ).to.be.array();
+        expect( result ).to.be.ofSize(1);
+        expect(result).to.be.equalTo(expected);
+      });
+
+      it('splits unquoted strings as `split(/\\s+/)`', function () {
+        const data = ' abc def    ghi ';
+        const expected = data.split(/\s+/);
+        const result = stringUtils.splitQuoted(data);
+        expect(result).to.be.equalTo(expected);
+      });
+
+      it('not splits spaces inside quoted strings`', function () {
+        const data = 'abc "def ghi" jkl';
+        const expected = ['abc', 'def ghi', 'jkl'];
+        const result = stringUtils.splitQuoted(data);
+        expect(result).to.be.equalTo(expected);
+      });
+
+      it('handles several spaces as one', function () {
+        const data = 'abc "def"  \"ghi\"   jkl';
+        const expected = ['abc', 'def', 'ghi', 'jkl'];
+        const result = stringUtils.splitQuoted(data);
+        expect(result).to.be.equalTo(expected);
+      });
+
+      it('handles escaped quote \\" as regular character', function () {
+        const data = 'abc \\"def\\" "ghi \\" jkl" mno pqr';
+        const expected = ['abc', '"def"', 'ghi " jkl', 'mno', 'pqr'];
+        const result = stringUtils.splitQuoted(data);
+        expect(result).to.be.equalTo(expected);
+      });
+
+    });
+
+    describe('templateLiteralsLike', function () {
+
+      it('is a function', function () {
+        assert(typeof stringUtils.templateLiteralsLike === 'function', 'Expect function');
+      });
+
+      it('handles some cases', function () {
+        const data = 'abc ${def} ghi';
+        const context = { def: 'fed' };
+        const expected = 'abc fed ghi';
+        const result = stringUtils.templateLiteralsLike(data, context);
+        expect(result).to.be.equal(expected);
+      });
+
+    });
 
   });
 
