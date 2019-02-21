cced9f33a1127214964b286ff3787639346e1fd3
Alexander <alykoshin@gmail.com>
2019-02-21 15:26:22 +0300

    refactor

--- demo/demo.sh
+++ demo/demo.sh
@@ -4,9 +4,9 @@
 #node cli.js lookup ENOENT
 #node cli.js confirm "Confirm [Y/N]?"
 #node cli.js keypress "Press any key... "
-#node cli.js gen_certs ./demo/gen-certs-config.json
+
+node cli.js gen_certs ./demo/gen-certs-config.json
 
 #node cli.js git queryClean
-
-node cli.js mongo backup ./demo/mongo-backup.json ./demo/mongo-restore.json
+#node cli.js mongo backup ./demo/mongo-backup.json ./demo/mongo-restore.json
 
