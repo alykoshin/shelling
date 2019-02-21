f49c39573b95e036774412042dfbc1c0894d107f
Alexander <alykoshin@gmail.com>
2019-02-21 13:28:10 +0300

    add git & mongo, rewrite a lot

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-var argv = require('minimist')(process.argv.slice(2));
-var pkg = require('./package.json');
-var shelling = require('./');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+
+const pkg = require('./package.json');
+const shelling = require('./');
 
 
 function help() {
@@ -21,7 +23,18 @@ function help() {
     '  lookup:',
     '    node node_modules/' + pkg.name + '/cli.js lookup NOENT',
     '  gen_certs:',
-    '    node node_modules/' + pkg.name + '/cli.js gen_certs ./demo/config.json',
+    '    node node_modules/' + pkg.name + '/cli.js gen_certs ./demo/gen-certs-config.json',
+    '  keypress:',
+    '    node node_modules/' + pkg.name + '/cli.js keypress "Press any key"',
+    '  confirm:',
+    '    node node_modules/' + pkg.name + '/cli.js keypress "Confirm [Y/N]?"',
+    '',
+    '  mongo backup:',
+    '    node node_modules/' + pkg.name + '/cli.js mongo backup ./demo/mongo-backup.json',
+    '  mongo backup+restore:',
+    '    node node_modules/' + pkg.name + '/cli.js mongo backup ./demo/mongo-backup.json ./demo/mongo-restore.json',
+    '  mongo restore:',
+    '    node node_modules/' + pkg.name + '/cli.js mongo restore ./demo/mongo-restore.json ',
     '',
   ].join('\n'));
 }
@@ -44,21 +57,62 @@ async function handleArgv() {
     process.exit(0);
 
   } else if (process.argv[ 2 ] === 'gen_certs') {
-    const res = await shelling.gen_certs(process.argv[3]);
+    const opensslActivity = new shelling.OpensslActivity();
+    const res = await opensslActivity.gen_certs(process.argv[3]);
+    //console.log('*', res);
+    process.exit(0);
+
+  } else if (process.argv[ 2 ] === 'git') {
+    const gitActivity = new shelling.GitActivity();
+    const action = process.argv[3];
+    if (!action) throw new Error('Expecting GitActivity action');
+    if (!gitActivity[action]) throw new Error('Invalid GitActivity action');
+    const res = await gitActivity[ action ]();
     //console.log('*', res);
     process.exit(0);
 
   } else if (process.argv[ 2 ] === 'lookup') {
     const code = process.argv[ 3 ];
-    console.log('*', shelling.lookup(code));
+    console.log('* RESULT:', shelling.lookupError(code));
     process.exit(0);
 
   } else if (process.argv[ 2 ] === 'keypress') {
-    console.log('*', await shelling.keypress('Press any key'));
+    const msg = process.argv[ 3 ] || 'Press any key...';
+    console.log('* RESULT:', await shelling.keypress(msg));
     process.exit(0);
 
   } else if (process.argv[ 2 ] === 'confirm') {
-    console.log('*', await shelling.confirm('Continue y/n?'));
+    const msg = process.argv[ 3 ] || 'Confirm [Y/N]?';
+    console.log('* RESULT:', await shelling.confirm(msg));
+    process.exit(0);
+
+  } else if (process.argv[2] === 'mongo' && process.argv[3] === 'backup') {
+    const backupConfigFile = process.argv[4];
+    const backupConfig = require(path.resolve(backupConfigFile));
+    const mongoUtils = new shelling.MongoActivity(backupConfig);
+
+    const backupDir = await mongoUtils.backup();
+
+    if (process.argv[5]) {
+      if (await shelling.confirm('Restore [Y/N]?')) {
+        const restoreConfigFile = process.argv[ 5 ];
+        const restoreConfig     = require(path.resolve(restoreConfigFile));
+        const mongoUtils        = new shelling.MongoActivity(restoreConfig);
+
+        await mongoUtils.restore(backupDir);
+      } else {
+        console.log('Skipped');
+      }
+    }
+
+    process.exit(0);
+
+  } else if (process.argv[2] === 'mongo' && process.argv[3] === 'restore') {
+    const configFile = process.argv[4];
+    const config = require(path.resolve(configFile));
+    const mongoUtils = new shelling.MongoActivity(config);
+
+    await mongoUtils.restore();
     process.exit(0);
 
   } else if (process.argv[ 2 ] === 'spawn') {
@@ -82,6 +136,8 @@ async function handleArgv() {
         process.exit(-1);
       })
     ;
+  } else {
+    throw new Error('Invalid command');
   }
 }
 
---
+++ demo/demo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#node cli.js spawn pwd
+#node cli.js lookup ENOENT
+#node cli.js confirm "Confirm [Y/N]?"
+#node cli.js keypress "Press any key... "
+#node cli.js gen_certs ./demo/gen-certs-config.json
+
+#node cli.js git queryClean
+
+node cli.js mongo backup ./demo/mongo-backup.json ./demo/mongo-restore.json
+
