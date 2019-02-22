d061ec85eeaf2e9c6d0de8032e3ff8a02c7f0291
Alexander <alykoshin@gmail.com>
2019-02-22 13:50:08 +0300

    add git initAndPush

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+this_file_dir=`dirname "$(readlink -f "$0")"`
+
 #node cli.js spawn pwd
 #node cli.js lookup ENOENT
 #node cli.js confirm "Confirm [Y/N]?"
@@ -12,7 +14,8 @@
 #node cli.js git readSpecificGitCredentials '{"hostname":"github.com","username":"alykoshin"}'
 #node cli.js git readGithubUsername
 #node cli.js git readGithubToken
-node cli.js git createGithubRepo '{"username":"alykoshin","name":"test","description":"test"}'
+#node cli.js git createGithubRepo '{"username":"alykoshin","name":"test","description":"test"}'
+node "$this_file_dir/../cli.js" git initAndPush '{"username":"alykoshin","repository":"test"}'
 
 #node cli.js mongo backup ./demo/mongo-backup.json ./demo/mongo-restore.json
 
